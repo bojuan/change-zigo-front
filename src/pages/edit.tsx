0204b1ccf60b134e7f570807ed5a94682af27f71
Bojuan <juanaciolalangui@hotmail.com>
2024-10-15 20:29:12 -0500

    feat: config pages

---
+++ src/pages/edit.tsx
@@ -0,0 +1,12 @@
+import Layout from "@/app/layout";
+import { Button } from "@/components/ui/button";
+
+export default function Edit() {
+  return (
+    <Layout>
+      <div>
+        <Button>Edit</Button>
+      </div>
+    </Layout>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Layout from "@/app/layout";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>
+        <Button>Init</Button>
+      </div>
+    </Layout>
+  );
+}
